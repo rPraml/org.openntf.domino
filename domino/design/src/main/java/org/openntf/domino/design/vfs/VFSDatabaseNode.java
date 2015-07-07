d85ccfde7c6b25331926010560c9cd44b99585e1
Manfred Steinsiek <manfred.steinsiek@foconis.de>
2015-07-07 10:21:37 +0200

    Missing method in interface VFSDatabaseNode added

--- domino/design/src/main/java/org/openntf/domino/design/vfs/VFSDatabaseNode.java
+++ domino/design/src/main/java/org/openntf/domino/design/vfs/VFSDatabaseNode.java
@@ -1,6 +1,10 @@
 package org.openntf.domino.design.vfs;
 
+import org.openntf.domino.Database;
 
 public interface VFSDatabaseNode extends VFSNode {
+
 	public void refresh();
+
+	public Database getDatabase();
 }
