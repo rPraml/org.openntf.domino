a2b5d2e7d23cc23e88aebedc09b65e8d09cb0ac8
Alexander Wagner <Alexander.Wagner@foconis.de>
2015-07-10 10:12:04 +0200

    refactored PackageScanner and PeriodicTask

--- domino/commons/src/main/java/org/openntf/domino/commons/utils/PackageScanner.java
+++ domino/commons/src/main/java/org/openntf/domino/commons/utils/PackageScanner.java
@@ -7,6 +7,12 @@ import java.util.Enumeration;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * Util-Class that can scan packages and find all classes in the package and its sub-packages.
+ * 
+ * @author Alexander Wagner, FOCONIS AG
+ *
+ */
 public class PackageScanner {
 
 	private final static char DOT = '.';
@@ -16,25 +22,42 @@ public class PackageScanner {
 	private final static String PACKAGE_ERROR_MSG = "Unable to get resources from path '%s'."
 			+ " It seams that the package '%s' does not exists.";
 
+	/**
+	 * Delivers all classes in the given package and its sub-packages.
+	 * 
+	 * @param packageName
+	 *            The Name of the given package
+	 * @return the classes as list
+	 */
 	public final static List<Class<?>> findClasses(final String packageName) {
-		final ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-		final String pathName = packageName.replace(DOT, SLASH);
-		final Enumeration<URL> resources;
+		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+		String pathName = packageName.replace(DOT, SLASH);
 		try {
-			resources = classLoader.getResources(pathName);
+			Enumeration<URL> resources = classLoader.getResources(pathName);
+			List<Class<?>> classes = new LinkedList<Class<?>>();
+
+			while (resources.hasMoreElements()) {
+				File file = new File(resources.nextElement().getFile());
+				classes.addAll(find(file, packageName));
+			}
+			return classes;
 		} catch (IOException e) {
 			throw new IllegalArgumentException(String.format(PACKAGE_ERROR_MSG, pathName, packageName), e);
 		}
-		final List<Class<?>> classes = new LinkedList<Class<?>>();
-		while (resources.hasMoreElements()) {
-			final File file = new File(resources.nextElement().getFile());
-			classes.addAll(find(file, packageName));
-		}
-		return classes;
 	}
 
+	/**
+	 * Helper method that returns the class of the current file in the package and the classes of its sub-packages recursively.
+	 * 
+	 * @param file
+	 *            The current file, that is propably a .class file
+	 * @param packageName
+	 *            The name of the given package
+	 * @return the classes as list
+	 */
 	protected final static List<Class<?>> find(final File file, final String packageName) {
-		final List<Class<?>> classes = new LinkedList<Class<?>>();
+		List<Class<?>> classes = new LinkedList<Class<?>>();
+
 		if (file.isDirectory()) {
 			for (File nestedFile : file.listFiles()) {
 				classes.addAll(find(nestedFile, packageName));
@@ -43,13 +66,14 @@ public class PackageScanner {
 		} else if (file.getName().endsWith(CLASS_SUFFIX) && !file.getName().contains("$")) {
 
 			String className = file.getAbsolutePath().replace(BACKSLASH, DOT);
-			final int beginIndex = className.indexOf(packageName);
+			int beginIndex = className.indexOf(packageName);
 			className = className.substring(beginIndex);
-			final int endIndex = className.length() - CLASS_SUFFIX.length();
+			int endIndex = className.length() - CLASS_SUFFIX.length();
 			try {
-				final String resource = className.substring(0, endIndex);
+				String resource = className.substring(0, endIndex);
 				classes.add(Class.forName(resource));
-			} catch (ClassNotFoundException ignore) {
+			} catch (ClassNotFoundException e) {
+				//ignore exception
 			}
 		}
 		return classes;
--- domino/xots/src/main/java/org/openntf/domino/xots/tasks/impl/PeriodicTask.java
+++ domino/xots/src/main/java/org/openntf/domino/xots/tasks/impl/PeriodicTask.java
@@ -1,24 +1,20 @@
 package org.openntf.domino.xots.tasks.impl;
 
-import org.openntf.domino.commons.IDateTime;
 
+/**
+ * Class that represents a periodic task with the className of the tasklet and the appropriate schedules.
+ * 
+ * @author Alexander Wagner, FOCONIS AG
+ *
+ */
 public class PeriodicTask {
 
 	private Class<?> taskletClass;
-	private IDateTime lastExecution;
 	private String[] schedule;
 
 	public PeriodicTask(final Class<?> taskletClassName, final String[] schedule) {
 		this.taskletClass = taskletClassName;
 		this.schedule = schedule;
-	}
-
-	public void setLastExecution(final IDateTime lastExecution) {
-		this.lastExecution = lastExecution;
-	}
-
-	public IDateTime getLastExecution() {
-		return lastExecution;
 	}
 
 	public Class<?> getTaskletClass() {
@@ -28,4 +24,5 @@ public class PeriodicTask {
 	public String[] getSchedule() {
 		return schedule;
 	}
+
 }
