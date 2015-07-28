992ee5a169e76ffca799f13a54c373227a3e3c20
Roland Praml <praml@foconis.de>
2015-07-28 13:53:05 +0200

    FIX: NonOsgiServiceLocator does not fail completely, if one service class could not be instantiiated

--- domino/commons/src/main/java/org/openntf/domino/commons/impl/NonOsgiServiceLocator.java
+++ domino/commons/src/main/java/org/openntf/domino/commons/impl/NonOsgiServiceLocator.java
@@ -6,13 +6,17 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.ServiceConfigurationError;
 import java.util.ServiceLoader;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.openntf.domino.commons.ServiceLocator;
 
 public class NonOsgiServiceLocator extends ServiceLocator {
 	@SuppressWarnings("rawtypes")
+	private static final Logger log_ = Logger.getLogger(NonOsgiServiceLocator.class.getName());
 	private static Map<Class, List> cache = new ConcurrentHashMap<Class, List>(); // in a NON-OSGI-Enviroment we may cache this
 
 	@Override
@@ -32,7 +36,11 @@ public class NonOsgiServiceLocator extends ServiceLocator {
 						ServiceLoader<T> loader = ServiceLoader.load(serviceClazz, cl);
 						Iterator<T> it = loader.iterator();
 						while (it.hasNext()) {
-							fret.add(it.next());
+							try {
+								fret.add(it.next());
+							} catch (ServiceConfigurationError se) {
+								log_.log(Level.SEVERE, se.getMessage(), se);
+							}
 						}
 					}
 
